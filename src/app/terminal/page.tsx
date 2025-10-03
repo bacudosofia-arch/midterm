a8a96181fbd128558943349325a89c662cfa0d75
bacudosofia-arch <bacudosofia@gmail.com>
2025-10-03 12:25:21 +0800

    Updated tsconfig.json

--- src/app/terminal/page.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-
-// Dynamic import to avoid SSR issues with client-side components
-const App = dynamic(() => import('../../App'), { ssr: false });
-
-export default function TerminalPage() {
-  return <App />;
-}
